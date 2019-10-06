2538e4d7b3bb6089d9ed1801588ceb494fb782ea
Paul Moscoso Garay <pauluni10@hotmail.com>
2019-10-06 14:05:47 +0200

    dbContext

---
+++ CleanArch.Infra.Data/Context/UniversityDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CleanArch.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Context
+{
+    class UniversityDbContext: DbContext
+    {
+        public UniversityDbContext(DbContextOptions options) : base(options) {
+
+        }
+
+        public DbSet<Course> Course { get; set; }
+
+        
+    }
+}
